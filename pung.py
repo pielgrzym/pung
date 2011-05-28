0bbf58c9ad6ad554fb2a527396975c788271a0c6
Jakub Nawalaniec <pielgrzym@prymityw.pl>
2011-05-28 19:04:16 +0200

    Added a feature: game over. Does not work well

--- pung.py
+++ pung.py
@@ -7,13 +7,20 @@ bg_col = 0,0,0
 screen = pygame.display.set_mode(size)
 
 class Loop(object):
-    def __init__(self, register=[]):
+    def __init__(self, register=[], interactive=[]):
         """
         Inicjalizacja głównej pętli programu
         """
     
         self.object_queue = register
+        self.interactive_queue = interactive
         self.start()
+
+    def process_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+            for game_object in self.interactive_queue:
+                game_object(event)
 
     def start(self):
         """
@@ -22,13 +29,8 @@ class Loop(object):
         """
     
         while 1:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT: sys.exit()
-                for game_object in self.object_queue:
-                    game_object(event)
-            for game_object in self.object_queue:
-                game_object()
             self.refresh()
+            self.process_events()
     
     def refresh(self):
         """
@@ -38,6 +40,7 @@ class Loop(object):
     
         screen.fill(bg_col)
         for game_object in self.object_queue:
+            game_object()
             screen.blit(game_object.image, game_object.rect)
         pygame.display.flip()
 
@@ -60,32 +63,41 @@ class Pad(Movable):
     def __call__(self,event=None):
         if not event: return
         if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
-            for x in range(30):
+            for x in range(60):
                 screen.blit(screen, self.rect, self.rect)
                 self.rect = self.rect.move(0,-2)
                 screen.blit(self.image, self.rect)
                 pygame.display.update(self.rect)
+                pygame.time.delay(1)
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
-            for x in range(30):
+            for x in range(60):
                 screen.blit(screen, self.rect, self.rect)
                 self.rect = self.rect.move(0,2)
                 screen.blit(self.image, self.rect)
                 pygame.display.update(self.rect)
+                pygame.time.delay(1)
 
 class Ball(Movable):
-    def __init__(self, start=[0,0]):
+    def __init__(self, start=[0,0], pad=None):
         self.image = pygame.image.load("img/ball.gif")
         self.rect = self.image.get_rect()
         self.move(start)
+        self.pad = pad
         self.speed = [2,2]
 
     def __call__(self, event=None):
-        if self.rect.left < 0 or self.rect.right > width:
+        if self.rect.right > width:
             self.speed[0] = -self.speed[0]
         if self.rect.top < 0 or self.rect.bottom > height:
             self.speed[1] = -self.speed[1]
+        if self.rect.left <= self.pad.rect.right:
+            if self.rect.y <= self.pad.rect.y and self.rect.y-100 <= self.pad.rect.y:
+                self.speed[0] = -self.speed[0]
+            elif self.rect.left <= 0:
+                sys.exit(0)
         self.move(self.speed)
 
-ball = Ball(start=[20,100])
+
 pad1 = Pad()
-loop = Loop(register=[ball,pad1,])
+ball = Ball(start=[20,100],pad=pad1)
+loop = Loop(register=[ball,pad1], interactive=[pad1,])
