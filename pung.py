75a59bfa2027c04ae6f46e8f7d6cd227ad8e3cae
Jakub Nawalaniec <pielgrzym@prymityw.pl>
2011-05-29 01:19:02 +0200

    Sprite rewrite! Now, this is what I call FAST :]

--- pung.py
+++ pung.py
@@ -1,100 +1,103 @@
 # -*- coding: utf-8 -*-
 import sys, pygame
-pygame.init()
-size = width, height = 800, 600
-bg_col = 0,0,0
 
-screen = pygame.display.set_mode(size)
-
-class Loop(object):
-    def __init__(self, register=[], interactive=[]):
-        """
-        Inicjalizacja głównej pętli programu
-        """
-    
-        self.object_queue = register
-        self.interactive_queue = interactive
-        self.start()
-
-    def process_events(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.exit()
-            for game_object in self.interactive_queue:
-                game_object(event)
-
-    def start(self):
-        """
-        To tutaj kręci się główna pętla :>
-    
-        """
-    
-        while 1:
-            self.refresh()
-            self.process_events()
-    
-    def refresh(self):
-        """
-        Odświeża ekran
-    
-        """
-    
-        screen.fill(bg_col)
-        for game_object in self.object_queue:
-            game_object()
-            screen.blit(game_object.image, game_object.rect)
-        pygame.display.flip()
-
-class Movable(object):
-    def move(self, coords):
-        """
-        Moves to desired coords
-    
-        """
-        x,y = coords
-        self.rect = self.rect.move(x,y)
-
-    def smooth_move(self, coords):
-        x,y = coords
-        for i in range(60):
-            self.move([x,y])
-            screen.blit(self.image, self.rect)
-            pygame.display.update(self.rect)
-            pygame.time.delay(1)
-
-class Pad(Movable):
-    def __init__(self, start=[0,0]):
+class SpritePad(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("img/pad.png")
+        self.image = self.image.convert()
         self.rect = self.image.get_rect()
-        self.move(start)
 
-    def __call__(self,event=None):
-        if not event: return
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
-            self.smooth_move([0,-2])
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
-            self.smooth_move([0,2])
+    def update(self):
+        """
+        docstring
+    
+        """
+    
+        pos = pygame.mouse.get_pos()[1]
+        self.rect.midleft = [0,pos]
 
-class Ball(Movable):
-    def __init__(self, start=[0,0], pad=None):
-        self.image = pygame.image.load("img/ball.gif")
-        self.rect = self.image.get_rect()
-        self.move(start)
+class SpriteBall(pygame.sprite.Sprite):
+    def __init__(self, pad=None):
+        pygame.sprite.Sprite.__init__(self)
         self.pad = pad
-        self.speed = [2,2]
+        self.image = pygame.image.load("img/ball.gif")
+        self.image = self.image.convert()
+        self.rect = self.image.get_rect()
+        screen = pygame.display.get_surface()
+        self.area = screen.get_rect()
+        self.rect.topleft = 10, 10
+        self.move = [5,5]
+        #self.move = [15,10]
 
-    def __call__(self, event=None):
-        if self.rect.right > width:
-            self.speed[0] = -self.speed[0]
-        if self.rect.top < 0 or self.rect.bottom > height:
-            self.speed[1] = -self.speed[1]
-        if self.rect.left <= self.pad.rect.right:
-            if self.rect.y >= self.pad.rect.y-50 and self.rect.y <= self.pad.rect.y+159:
-                self.speed[0] = -self.speed[0]
-            elif self.rect.left <= 0:
-                sys.exit(0)
-        self.move(self.speed)
+    def update(self):
+        self._fly() # leć, kurwa, leć!
 
+    def _fly(self):
+        """
+        Latanie jakie jest, każdy widzi
+    
+        """
+    
+        newpos = self.rect.move(self.move[0], self.move[1])
+        if not self.area.contains(newpos):
+            if self.rect.right > self.area.right:
+                self.move[0] = -self.move[0]
+            if self.rect.top < self.area.top or self.rect.bottom > self.area.bottom:
+                self.move[1] = -self.move[1]
+            if self.rect.left < self.pad.rect.right:
+                if self.rect.y >= self.pad.rect.y-50 and self.rect.y <= self.pad.rect.y+159:
+                    self.move[0] = -self.move[0]
+                else:
+                    sys.exit(0)
+            #if self.rect.left < self.area.left or self.rect.right > self.area.right:
+                #self.move[0] = -self.move[0]
+                #self.move[1] = -self.move[1]
+            newpos = self.rect.move((self.move[0], self.move[1]))
+            self.image = pygame.transform.flip(self.image, 1, 0)
+        self.rect = newpos
 
-pad1 = Pad()
-ball = Ball(start=[20,100],pad=pad1)
-loop = Loop(register=[ball,pad1], interactive=[pad1,])
+def main():
+    """
+    docstring
+
+    """
+    pygame.init()
+    size = width, height = 800, 600
+    bg_col = 0,0,0
+
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("Pung - Pielgrzym's pong")
+    pygame.mouse.set_visible(0)
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill((0,0,0))
+
+    if pygame.font:
+        font = pygame.font.Font(None, 36)
+        text = font.render("PUNG motherfucker", 1, (10,10,10))
+        textpos = text.get_rect(centerx=background.get_width()/2)
+        background.blit(text, textpos)
+
+    screen.blit(background, (0,0))
+    pygame.display.flip()
+    pad = SpritePad()
+    ball = SpriteBall(pad=pad)
+    allsprites = pygame.sprite.RenderPlain((pad,ball))
+    clock = pygame.time.Clock()
+
+    while 1:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                return
+        allsprites.update()
+        screen.blit(background, (0, 0))
+        allsprites.draw(screen)
+        pygame.display.flip()
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
