938d400e42646d929972a236d26c637969a7af6f
Jakub Nawalaniec <pielgrzym@prymityw.pl>
2011-05-29 17:45:57 +0200

    Ball & pad can be relative to sth & playfield

--- pung.py
+++ pung.py
@@ -2,11 +2,12 @@
 import pygame, os
 
 class SpritePad(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, relative_to=None):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(os.path.join("data","pad.png"))
         self.image = self.image.convert()
         self.rect = self.image.get_rect()
+        self.relative_to = relative_to
         self.hitzones = [ 
                 pygame.Rect(self.rect.left, self.rect.top, 20, 20),
                 pygame.Rect(self.rect.left, self.rect.top+140, 20, 20),
@@ -19,21 +20,26 @@ class SpritePad(pygame.sprite.Sprite):
 
     def update(self):
         pos = pygame.mouse.get_pos()[1]
-        self.rect.midleft = [0, pos]
+        if pos < self.relative_to.top+(self.rect.height/2):
+            self.rect.top = self.relative_to.top
+        elif pos > self.relative_to.bottom-(self.rect.height/2):
+            self.rect.bottom = self.relative_to.bottom
+        else:
+            self.rect.midleft = [0, pos]
         self.hitzones[0].top = self.rect.top
         self.hitzones[1].bottom = self.rect.bottom
 
 class SpriteBall(pygame.sprite.Sprite):
-    def __init__(self, pad=None):
+    def __init__(self, pad_left=None, relative_to=None):
         pygame.sprite.Sprite.__init__(self)
-        self.pad = pad
+        self.pad_left = pad_left
         self.image = pygame.image.load(os.path.join("data", "trollface.png"))
         self.image = self.image.convert_alpha()
         colorkey = self.image.get_at((0,0))
         self.image.set_colorkey(colorkey, pygame.RLEACCEL)
         self.rect = self.image.get_rect()
         screen = pygame.display.get_surface()
-        self.area = screen.get_rect()
+        self.relative_to = relative_to
         self.rect.topleft = 30, screen.get_size()[1]/2 # start pos
         self.move = [4,6] # movement vector
         #self.move = [15,10]
@@ -48,32 +54,32 @@ class SpriteBall(pygame.sprite.Sprite):
         """
     
         newpos = self.rect.move(self.move[0], self.move[1])
-        if not self.area.contains(newpos):
-            if self.rect.right > self.area.right:
+        if not self.relative_to.contains(newpos):
+            if self.rect.right > self.relative_to.right:
                 self.move[0] = -self.move[0]
                 if self.move[0] > 15:
                     self.move[0] -= 10
                 if self.move[1] > 10:
                     self.move[1] -= 5
                 self.image = pygame.transform.flip(self.image, 1, 0)
-            if self.rect.top < self.area.top or self.rect.bottom > self.area.bottom:
+            if self.rect.top < self.relative_to.top or self.rect.bottom > self.relative_to.bottom:
                 self.move[1] = -self.move[1]
                 if self.move[0] > 15:
                     self.move[0] -= 10
                 if self.move[1] > 10:
                     self.move[1] -= 5
-            #if self.pad.rect.colliderect(self.rect):
-            if self.rect.colliderect(self.pad.rect):
+            #if self.pad_left.rect.colliderect(self.rect):
+            if self.rect.colliderect(self.pad_left.rect):
                 self.move[0] = -self.move[0]
-                if self.rect.colliderect(self.pad.hitzones[0]):
+                if self.rect.colliderect(self.pad_left.hitzones[0]):
                     print 'top'
                     self.move[1] -= 5
-                elif self.rect.colliderect(self.pad.hitzones[1]):
+                elif self.rect.colliderect(self.pad_left.hitzones[1]):
                     print 'bot'
                     self.move[1] += 5
                 self.image = pygame.transform.flip(self.image, 1, 0)
             else:
-                if self.rect.left < self.area.left:
+                if self.rect.left < self.relative_to.left:
                     # begin fadeout
                     from pygame import surfarray
                     import numpy as N
@@ -120,7 +126,7 @@ def main():
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption("Trollface pung. Enjoy. v0.40")
     pygame.mouse.set_visible(0)
-    background = pygame.Surface(screen.get_size())
+    background = pygame.Surface((screen.get_size()))
     background = background.convert()
     background.fill((0,0,0))
 
@@ -132,9 +138,16 @@ def main():
 
     screen.blit(background, (0,0))
     pygame.display.flip()
-    pad = SpritePad()
-    ball = SpriteBall(pad=pad)
-    allsprites = pygame.sprite.RenderUpdates((pad,ball))
+
+    playarea = pygame.image.load(os.path.join("data", "playfield.png"))
+    playarea.convert_alpha()
+    playarea_rect = playarea.get_rect()
+    playarea_rect.top = 50
+    background.blit(playarea, playarea_rect)
+
+    pad_left = SpritePad(relative_to=playarea_rect)
+    ball = SpriteBall(pad_left=pad_left, relative_to=playarea_rect)
+    allsprites = pygame.sprite.RenderUpdates((pad_left,ball))
     clock = pygame.time.Clock()
 
     while 1:
